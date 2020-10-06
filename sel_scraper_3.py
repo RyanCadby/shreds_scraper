2a0b07fcc97d44466a6f3c16bd6392a7c02173ee
Ryan Cadby <cadby.2@gmail.com>
2020-10-05 20:30:14 -0600

    added page not found exception

--- sel_scraper_3.py
+++ sel_scraper_3.py
@@ -85,9 +85,24 @@ for i in range(page_total): # for each page of 50 riders
                 print('SUCCESS: on rider page')
         except ex:
             print('FAIL: could not find rider link')
-            driver.execute_script("location.reload()")
-            profile_link = driver.find_elements_by_class_name('ranking-table-link')[i]
-            profile_link.click()
+            # check for page not found
+            profile_soup_test = BeautifulSoup(driver.page_source, 'html.parser')
+            fourhundo = profile_soup_test.find('div', attrs={'id': 'result-table-points-list-ss'}).h1.get_text()
+            print(fourhundo)
+            if fourhundo == 'Page Not Found':
+                # go back to initial page
+                driver.execute_script("window.history.go(-1)")
+                print('FAIL: 404 page not found')
+                #start new line for new rider profile
+                f.write("\n")
+                print('EXIT: write new line in csv')
+                profile_link = driver.find_elements_by_class_name('ranking-table-link')[i+1]
+                print('EXIT: find new rider')
+                profile_link.click()
+            else:
+                driver.execute_script("location.reload()")
+                profile_link = driver.find_elements_by_class_name('ranking-table-link')[i]
+                profile_link.click()
         except IndexError:
             rider_wait = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "rider-label")))
             print('SUCCESS: found rider name on rider profile page')   
@@ -137,9 +152,7 @@ for i in range(page_total): # for each page of 50 riders
 
         #get rider points
         rider_point_soup = profile_soup.find('div', attrs={'id': 'result-table-points-list-ss'})
-        if rider_point_soup is None:
-            profile[1] = ''
-        else:
+        if rider_point_soup is not None:
             rider_point_cont = rider_point_soup.ul.find_all('li')
             point_counter = 1
             for litag in rider_point_cont:
@@ -147,72 +160,76 @@ for i in range(page_total): # for each page of 50 riders
                 rider_point_info = rider_point_data.split(':', 1)
                 profile[point_counter] = rider_point_info[1]
                 point_counter += 1
-
-            # get rider sponsors
-            rider_sponsor_soup = profile_soup.find('div', attrs={'class': 'sponsor-list'})
-            if rider_sponsor_soup is None:
-                profile[3] = ''
-            else:
-                rider_sponsor_soup_conf = rider_sponsor_soup.ul.find_all('li')
-                sponsors = ''
-                for litag in rider_sponsor_soup_conf:
-                    sponsor_item = litag.text.strip()
-                    sponsors += sponsor_item + ' | '
-
-                profile[3] = sponsors
+        else:
+            profile[1] = ''
+            print('FAIL: no slopestyle score')
+            
 
 
+        # get rider sponsors
+        rider_sponsor_soup = profile_soup.find('div', attrs={'class': 'sponsor-list'})
+        if rider_sponsor_soup is None:
+            profile[3] = ''
+        else:
+            rider_sponsor_soup_conf = rider_sponsor_soup.ul.find_all('li')
+            sponsors = ''
+            for litag in rider_sponsor_soup_conf:
+                sponsor_item = litag.text.strip()
+                sponsors += sponsor_item + ' | '
 
-            profile_code_soup = profile_soup.find_all('ul', attrs={'class': 'plain-list'})
-            for ultag in profile_code_soup:
-                profile_li = ultag.find_all('li')
+            profile[3] = sponsors
 
-                for litag in profile_li:
-                    # get data
-                    profile_info = litag.getText()
-                    # clean data
-                    profile_info = profile_info.replace("'", 'ft.')
-                    profile_info = profile_info.replace('"', 'in.')
-                    profile_info = profile_info.replace(',', '|')
 
-                    profile_type, profile_stat = profile_info.split(":", 1)
-                    profile_type = profile_type.lower()
-                    profile_stat = profile_stat.strip()
+        profile_code_soup = profile_soup.find_all('ul', attrs={'class': 'plain-list'})
+        for ultag in profile_code_soup:
+            profile_li = ultag.find_all('li')
 
-                    # check nationality
-                    if profile_type == 'age':
-                        profile[4]=profile_stat
-                    elif profile_type == 'nationality':
-                        profile[5]=profile_stat
-                    elif profile_type == 'stance':
-                        profile[6]=profile_stat
-                    elif profile_type == 'height':
-                        profile[7]=profile_stat
-                    elif profile_type == 'residence':
-                        profile[8]=profile_stat
-                    elif profile_type == 'home resort':
-                        profile[9]=profile_stat
-                    elif profile_type == 'website':
-                        profile[10]=profile_stat
-                    elif profile_type == 'facebook':
-                        profile[11]=profile_stat
-                    elif profile_type == 'twitter':
-                        profile[12]=profile_stat
-                    elif profile_type == 'instagram':
-                        profile[13]=profile_stat
-                    else:
-                        pass
+            for litag in profile_li:
+                # get data
+                profile_info = litag.getText()
+                # clean data
+                profile_info = profile_info.replace("'", 'ft.')
+                profile_info = profile_info.replace('"', 'in.')
+                profile_info = profile_info.replace(',', '|')
 
-            profile_str = ', '.join(str(x) for x in profile)
+                profile_type, profile_stat = profile_info.split(":", 1)
+                profile_type = profile_type.lower()
+                profile_stat = profile_stat.strip()
 
-            print('PROFILE STRING: ' + profile_str)
-            f.write(profile_str)
+                # check nationality
+                if profile_type == 'age':
+                    profile[4]=profile_stat
+                elif profile_type == 'nationality':
+                    profile[5]=profile_stat
+                elif profile_type == 'stance':
+                    profile[6]=profile_stat
+                elif profile_type == 'height':
+                    profile[7]=profile_stat
+                elif profile_type == 'residence':
+                    profile[8]=profile_stat
+                elif profile_type == 'home resort':
+                    profile[9]=profile_stat
+                elif profile_type == 'website':
+                    profile[10]=profile_stat
+                elif profile_type == 'facebook':
+                    profile[11]=profile_stat
+                elif profile_type == 'twitter':
+                    profile[12]=profile_stat
+                elif profile_type == 'instagram':
+                    profile[13]=profile_stat
+                else:
+                    pass
 
-            # go back to initial page
-            driver.execute_script("window.history.go(-1)")
+        profile_str = ', '.join(str(x) for x in profile)
 
-            #start new line for new rider profile
-            f.write("\n")
+        print('PROFILE STRING: ' + profile_str)
+        f.write(profile_str)
+
+        # go back to initial page
+        driver.execute_script("window.history.go(-1)")
+
+        #start new line for new rider profile
+        f.write("\n")
 
      # navigate to link
     page_next = driver.find_element_by_class_name('next')
