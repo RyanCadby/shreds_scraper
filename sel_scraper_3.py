679e57a3e562ba34858a0dddd97fc123d4418783
Ryan <ryan.cadby@gmail.com>
2020-10-08 12:29:00 -0600

    added venv activate instructions

--- sel_scraper_3.py
+++ sel_scraper_3.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException as ex
 # import time
 from bs4 import BeautifulSoup
+# source venv/bin/activate
 
 # import pandas as pd
 # import re
